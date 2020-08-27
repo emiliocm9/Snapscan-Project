3033482431f6ff704e3e071ed5f07a475a6cbd68
emiliocm9 <emilio.contreras97@gmail.com>
2020-08-27 16:44:24 -0500

    style: index page with style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_190312) do
+ActiveRecord::Schema.define(version: 2020_08_27_182928) do
+
+  create_table "groups", force: :cascade do |t|
+    t.integer "creator_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_groups_on_creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
   before(:each) do
-    @user = assign(:user, User.create!(
-                            name: 'Name'
-                          ))
+    @user = assign(:user, User.create!(name: 'Name'))
   end
 
   it 'renders attributes in <p>' do
