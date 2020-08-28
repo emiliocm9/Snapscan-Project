688997de0d6624c33c9b3dd0fcfaed051f805cf4
emiliocm9 <emilio.contreras97@gmail.com>
2020-08-28 13:24:58 -0500

    docs: bycrip gem install

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_221236) do
+ActiveRecord::Schema.define(version: 2020_08_28_173838) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_221236) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
